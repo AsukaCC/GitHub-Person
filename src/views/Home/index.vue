427ecb3deb227f254a752608446e4a6d1bf840b5
asukacc <plutavian@gmail.com>
2024-10-11 02:34:08 +0800

    update

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,8 +2,13 @@
 import LightBox from '@/components/LightBox.vue';
 </script>
 <template>
-  <div>
+  <div class="w-[100vw] h-[100vh] relative">
     <LightBox />
+    <div class="absolute bottom-0 w-full p-4 text-center">
+      <a class="text-gray-500" href="https://beian.miit.gov.cn/" target="_blank"
+        >蜀ICP备2023024939号-1</a
+      >
+    </div>
   </div>
 </template>
 
